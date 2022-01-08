a5271d3595742ba41fe7ecf6d7a7ad799d251507
olayode <aolayode@getcarbon.co>
2022-01-08 20:47:02 +0100

    implement template inheritance

--- EAC/base/views.py
+++ EAC/base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': "check1"},
+    {'id': 2, 'name': "check2"},
+    {'id': 3, 'name': "check3"},
+]
+
+
+def home(req):
+    return render(req, 'base/home.html', {'rooms': rooms})
+
+
+def room(req):
+    return render(req, 'base/room.html', {'rooms': rooms})
