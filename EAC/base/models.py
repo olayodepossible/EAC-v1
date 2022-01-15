28e5bfa318e85bf7fef20c9560c4fa62e17eff12
olayode <aolayode@getcarbon.co>
2022-01-15 15:49:09 +0100

    implement all the CRUD operation with MySQL

--- EAC/base/models.py
+++ EAC/base/models.py
@@ -4,14 +4,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Topic(models.Model):
+class BaseModel(models.Model):
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Topic(BaseModel):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Room(models.Model):
+class Room(BaseModel):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)  # null = false by default
@@ -20,12 +27,15 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
 
 
 # Message Model with One(Room)-to-Many relationship
-class Message(models.Model):
+class Message(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
--- EAC/base/views.py
+++ EAC/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RoomForm
+from .models import Room, Topic
 
 # Create your views here.
 
@@ -11,18 +12,46 @@ rooms = [
 
 
 def home(req):
-    context = {'rooms': rooms}
+    req_param = req.GET.get('q') if req.GET.get('q') != None else ''
+    class_rooms = Room.objects.filter(topic__name__icontains=req_param)  # icontains = search with ignoreCase
+    class_topics = Topic.objects.all()
+    context = {'rooms': class_rooms, 'topics': class_topics}
     return render(req, 'base/home.html', context)
 
 
 def room(req, key):
-    class_room = None
-    for i in rooms:
-        if i['id'] == int(key):
-            class_room = i
+    class_room = Room.objects.get(id=key)
+
     context = {'room': class_room}
     return render(req, 'base/room.html', context)
 
-def createRoom(req):
-    context = {}
+
+def create_room(req):
+    form = RoomForm()
+    if req.method == 'POST':
+        form = RoomForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form, 'topics': form['topic']}
     return render(req, 'base/room_form.html', context)
+
+
+def update_room(req, key):
+    class_room = Room.objects.get(id=key)
+    form = RoomForm(instance=class_room)
+    if req.method == 'POST':
+        form = RoomForm(req.POST, instance=class_room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(req, 'base/room_form.html', context)
+
+
+def delete_room(req, key):
+    class_room = Room.objects.get(id=key)
+    if req.method == 'POST':
+        class_room.delete()
+        return redirect('home')
+    return render(req, 'base/delete.html', {'obj': class_room})
